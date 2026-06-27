f3de25aa044214e19f6bd302a6540c9431042f93
indoguy23 <devcorp25@gmail.com>
2026-06-27 13:54:55 +0530

    Refactor Navbar component: enhance scroll effect, improve styles, and update structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,45 @@
-import { useState } from 'react';
-import './Navbar.scss'
-import { navLinks } from './navbarData'
-import { HiOutlineMenuAlt3, HiX } from 'react-icons/hi';
+import { useEffect, useState } from "react";
+import { HiOutlineMenuAlt3, HiX } from "react-icons/hi";
+
+import "./Navbar.scss";
+import { navLinks } from "./navbarData";
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <header className='navbar'>
+        <header className={`navbar ${isScrolled ? "navbar--scrolled" : ""}`}>
             <div className="navbar__container">
 
-                {/* LOGO */}
+                {/* Logo */}
                 <a href="#home" className="navbar__logo">
                     Aryan<span>.</span>
                 </a>
 
-                {/* Desktop Navigation */}
+                {/* Navigation */}
                 <nav className={`navbar__nav ${isMenuOpen ? "active" : ""}`}>
                     <ul className="navbar__menu">
+
                         {navLinks.map((item) => (
-                            <li key={item.id}>
+                            <li
+                                key={item.id}
+                                className="navbar__item"
+                            >
                                 <a
+                                    className="navbar__link"
                                     href={item.href}
                                     onClick={() => setIsMenuOpen(false)}
                                 >
@@ -27,23 +47,31 @@ const Navbar = () => {
                                 </a>
                             </li>
                         ))}
+
                     </ul>
 
-                    <button className="navbar__button">
+                    <button
+                        type="button"
+                        className="navbar__button"
+                    >
                         Resume
                     </button>
                 </nav>
 
-                {/* Mobile Menu Icon */}
+                {/* Mobile Toggle */}
+
                 <button
+                    type="button"
+                    aria-label="Toggle navigation"
                     className="navbar__toggle"
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
                 >
                     {isMenuOpen ? <HiX /> : <HiOutlineMenuAlt3 />}
                 </button>
+
             </div>
         </header>
-    )
-}
+    );
+};
 
 export default Navbar;
